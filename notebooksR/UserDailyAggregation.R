ffb2cf2e82395f0f19f5c52e17696b49ecab0b4a
nixiasancy <nixia92@gmail.com>
2021-12-13 11:00:37 +0100

    UserDailyAggregation
    UserDailyAggregation

---
+++ notebooksR/UserDailyAggregation.R
@@ -0,0 +1,4 @@
+setwd('C:/Users/njohn/OneDrive - IESEG/Semester 1/Business Reporting Tool_Open Source/Group Project/Group assignment folder-20211211/')
+load('DataGroupAssignment.Rdata')
+summary(UserDailyAggregation)
+sum(is.na(PokerChipConversions))
