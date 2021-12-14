b0bd2f5ebc51ce18e4ed794c0aeb9b52ee19bbc4
nixiasancy <nixia92@gmail.com>
2021-12-14 10:53:07 +0100

    With RFM
    Need to check on Monetary and RFM score value calculation..

--- notebooksR/UserDailyAggregation.R
+++ notebooksR/UserDailyAggregation.R
@@ -1,5 +1,8 @@
 setwd('C:/Users/njohn/OneDrive - IESEG/Semester 1/Business Reporting Tool_Open Source/Group Project/Group assignment folder-20211211/')
-library(dplyr)
+
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("formattable")) install.packages("formattable")
+options(scipen = 20)
 load('DataGroupAssignment.Rdata')
 summary(UserDailyAggregation)
 #write.csv(UserDailyAggregation,'UserDailyAggregation.csv')
@@ -33,11 +36,33 @@ UserDailyAggregation_3 <- UserDailyAggregation_2 %>%
 
 
 UserDailyAggregation_3$count <- UserDailyAggregation_2 %>% count(UserID)
+####Recency and Frequency calculateion
+#Get max date
+recency_Date <- max(UserDailyAggregation_2$Date)
+
+#calculate recency and frequency
+UserDailyAggregation_recency <- UserDailyAggregation_2 %>% 
+                                    group_by(UserID) %>% 
+                                summarise(recency = as.numeric(recency_Date - max(Date)),
+                                          frequency = n())
+#calculate monetary value check
+UserDailyAggregation_value <- PokerChipConversions %>%
+                                  group_by(UserID) %>%
+                                    filter(TransType == 124)  %>%
+                                      summarise(monetary_value = sum(TransAmount)) 
+
+#Merge the tables to get recency,frequency and lifetime value
+UserDailyAggregation_rfm <- left_join(UserDailyAggregation_recency,UserDailyAggregation_value,by='UserID')
+
+#Fill na with 0s
+UserDailyAggregation_rfm[is.na(UserDailyAggregation_rfm)] = 0
+
+#calculate RFM Score
+UserDailyAggregation_rfm <- UserDailyAggregation_rfm %>%
+                                  mutate(rfm_score = recency + frequency + monetary_value)
+
+#Scale RFM score
+UserDailyAggregation_rfm$rfm_score <- normalize(UserDailyAggregation_rfm$rfm_score,min = 0,max = 1)
 
 
-UserDailyAggregation_4 <- UserDailyAggregation_2 %>% 
-                              group_by(UserID,ProductID) %>%
-                                  slice(1)
-
-
-                                  
+UserDailyAggregation_3 <- left_join(UserDailyAggregation_3,UserDailyAggregation_rfm,by='UserID')
